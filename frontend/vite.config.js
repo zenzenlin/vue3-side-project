4509af954e1005580bf8014c1f976194915a65a6
zenzenlin <poietgr0107@gmail.com>
2022-12-26 20:13:44 +0800

    初始建置

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  // 前後端運行路徑端口不同，發送請求會有跨域問題
+  server: {
+    proxy: {
+      // url 傳送 /api 開頭的請求會帶上至設定的路徑後面進行發送
+      "/api": "http://localhost:5173",
+      "/upload": "http://localhost:5173",
+    },
+  },
+});
